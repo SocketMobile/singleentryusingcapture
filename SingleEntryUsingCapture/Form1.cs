90ba604cd2311a5c976b9a5e2521ab15b571be72
Eric Glaenzer <ericg@socketmobile.com>
2018-10-08 13:43:07 -0400

    remove the unused using statements

--- SingleEntryUsingCapture/Form1.cs
+++ SingleEntryUsingCapture/Form1.cs
@@ -8,15 +8,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 //1- add the SocketMobile Capture namespace
 using SocketMobile.Capture;
 
